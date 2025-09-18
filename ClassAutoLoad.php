97d9f9e93c81df986e0497e38dd15d42eda17100
bravintoo <167431403+bravin23409@users.noreply.github.com>
2025-09-18 15:54:49 +0300

    Git & Github Exercise

--- ClassAutoLoad.php
+++ ClassAutoLoad.php
@@ -1,16 +1,23 @@
 <?php
+
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require 'Plugins/PHPMailer/vendor/autoload.php';
 require 'conf.php';
-// Autoload classes from specified directories  
-$directory = ["Forms", "Global", "Layouts"];
-spl_autoload_register(function($className) use ($directory) {
-    foreach($directory as $dir) {
-        $file = __DIR__. "/$dir/" . $className . ".php";
-        if(file_exists($file)) {
+
+// autoload classes from specified directories
+$directory = ["Forms", "Globals", "Layouts"];
+
+spl_autoload_register(function ($class_name) use ($directory) {
+    foreach ($directory as $dir) {
+        $file = __DIR__ . "/$dir/" . $class_name . '.php';
+        if (file_exists($file)) {
             require_once $file;
             return;
         }
     }
-}); 
-// create instances of classes without manually including their files
-$Layouts = new layouts($conf);
-$Forms = new forms();
+});
+
+// create an instance of the class
+$ObjSendMail = new SendMail();
+$ObjLayouts = new layouts($conf);
+$ObjForms = new forms();
--- conf.php
+++ conf.php
@@ -13,3 +13,12 @@ $conf['db_host'] = "localhost";
 $conf['db_user'] = "root";
 $conf['db_pass'] = "182064";
 $conf['db_name'] = "bravin";
+
+
+// Email configuration
+$conf['mail_type'] = "smtp"; // options: smtp, sendmail, mail
+$conf['smtp_host'] = "smtp.gmail.com";
+$conf['smtp_user'] = "bravin.too@strathmore.edu";
+$conf['smtp_pass'] = "qipn surn zrsc avtu";
+$conf['smtp_port'] = 465;
+$conf['smtp_secure'] = "ssl"; // options: ssl, tls
